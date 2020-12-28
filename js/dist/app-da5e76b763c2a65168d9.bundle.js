5a3c123cc1ea7f3ee2e45ba4d0034210aec46c3b
awongh <akira@awongh.com>
2020-12-29 01:04:31 +0800

    ignore dist folder

--- js/dist/app-da5e76b763c2a65168d9.bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!*************************!*\
-  !*** ./js/src/index.js ***!
-  \*************************/
-console.log("yay works");
-alert('hello!');
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9mdWxsLXN0YWNrLW1vZGFsLXN3ZTEvLi9qcy9zcmMvaW5kZXguanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyIsImFsZXJ0Il0sIm1hcHBpbmdzIjoiOzs7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLFdBQVo7QUFDQUMsS0FBSyxDQUFDLFFBQUQsQ0FBTCxDIiwiZmlsZSI6ImFwcC1kYTVlNzZiNzYzYzJhNjUxNjhkOS5idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcInlheSB3b3Jrc1wiKTtcbmFsZXJ0KCdoZWxsbyEnKTtcbiJdLCJzb3VyY2VSb290IjoiIn0=
