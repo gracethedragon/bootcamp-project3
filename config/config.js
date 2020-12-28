f016739f329f8e693b0032a5fd8922374a2137a0
awongh <akira@awongh.com>
2020-12-29 02:45:26 +0800

    more config sequelize

--- config/config.js
+++ config/config.js
@@ -8,8 +8,6 @@ module.exports = {
   },
   production: {
     use_env_variable: "DATABASE_URL",
-    dialect: 'postgres',
-    protocol: 'postgres',
     dialectOptions: {
       ssl: { // https://github.com/sequelize/sequelize/issues/12083
         require: true,
