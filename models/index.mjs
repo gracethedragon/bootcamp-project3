666d542844ca9090f33ab7cbffe65f13b372872f
awongh <akira@awongh.com>
2020-12-29 02:20:12 +0800

    parse DB URL by hand in model index

--- models/index.mjs
+++ models/index.mjs
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import allConfig from '../config/config.js';
+import url from 'url';
 
 import itemModel from './item.mjs';
 
@@ -12,7 +13,20 @@ const db = {};
 let sequelize;
 
 if( env === 'production' ){
-  sequelize = new Sequelize(process.env.DATABASE_URL, config);
+
+  const DATABASE_URL = process.env.DATABASE_URL;
+  const db_url = url.parse(DATABASE_URL);
+  const username   = db_url.auth.substr(0, db_url.auth.indexOf(':'));
+  const password   = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
+  const dbName     = db_url.path.slice(1);
+
+  const host   = db_url.hostname
+  const port   = db_url.port;
+
+  config.host = host;
+  config.port = port;
+
+  sequelize = new Sequelize(dbName, username, password, config);
 }else{
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
