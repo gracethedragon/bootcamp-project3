9c7401239c504b71b16dd2bc0053a6f3e674b540
awongh <akira@awongh.com>
2020-12-29 11:13:49 +0800

    more pg configs

--- config/config.js
+++ config/config.js
@@ -7,7 +7,6 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: "DATABASE_URL",
     database: 'gosh_dang_matter_flipping',
     dialectOptions: {
       ssl: { // https://github.com/sequelize/sequelize/issues/12083
