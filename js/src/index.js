fba94254e06ab5b1a1d67b7f77a09e2947603480
awongh <akira@awongh.com>
2020-12-29 12:09:02 +0800

    add AJAX and SCSS

--- js/src/index.js
+++ js/src/index.js
@@ -1,2 +1,24 @@
-console.log("yay works");
-alert('hello!');
+import axios from 'axios';
+import './styles.scss';
+
+// Make a request for all the items
+axios.get('/items')
+  .then(function (response) {
+    // handle success
+    console.log(response.data.items);
+
+    const itemCont = document.createElement('div');
+
+    const itemEls = response.data.items.forEach( item =>{
+      const itemEl = document.createElement('div');
+      itemEl.innerText = JSON.stringify(item)
+      itemEl.classList.add('item');
+      document.body.appendChild( itemEl );
+    });
+
+    document.body.appendChild( itemCont );
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
