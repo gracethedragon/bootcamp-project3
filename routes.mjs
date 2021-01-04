0678cc864bd6e6860c81bad8ac47b6d3603f33ad
awongh <akira@awongh.com>
2021-01-04 18:49:44 +0800

    add back in full example front end

--- routes.mjs
+++ routes.mjs
@@ -1,9 +1,15 @@
-import { resolve } from 'path';
 import db from './models/index.mjs';
+import items from './controllers/items.mjs'
+import {resolve} from 'path';
 
-export default function routes(app) {
+export default function routes( app ){
+
+  const ItemsController = items(db);
+
+  app.get('/items', ItemsController.index);
+
   // special JS page. Include the webpack index.html file
-  app.get('/home', (request, response) => {
-    response.sendFile(resolve('dist', 'main.html'));
+  app.get('/home', (request,response) => {
+    response.sendFile(resolve('dist', 'main.html'))
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
+import axios from 'axios';
 import './styles.scss';
-console.log('hello');
+
+// Make a request for all the items
+axios.get('/items')
+  .then(function (response) {
+    // handle success
+    console.log(response.data.items);
+
+    const itemCont = document.createElement('div');
+
+    const itemEls = response.data.items.forEach( item =>{
+      const itemEl = document.createElement('div');
+      itemEl.innerText = JSON.stringify(item)
+      itemEl.classList.add('item');
+      document.body.appendChild( itemEl );
+    });
+
+    document.body.appendChild( itemCont );
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
