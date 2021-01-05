ac3d0aaed600d882dcebdfb28e9efefe21a989e8
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-05 13:02:19 +0800

    add comments to index.mjs, lint repo

--- routes.mjs
+++ routes.mjs
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 import db from './models/index.mjs';
 
-export default function routes(app) {
+export default function bindRoutes(app) {
   // special JS page. Include the webpack index.html file
   app.get('/home', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
