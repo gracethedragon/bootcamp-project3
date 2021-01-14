b1c3c6d06543af0ba85ab8150f936926a2447d7e
awongh <akira@awongh.com>
2021-01-14 10:07:03 +0800

    fix repeated common import

--- webpack_conf/webpack.dev.js
+++ webpack_conf/webpack.dev.js
@@ -1,9 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   entry: {
