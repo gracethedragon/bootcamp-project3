614284b022cbeab64e81562c78669e2d0dbb4983
awongh <akira@awongh.com>
2021-03-26 10:56:12 +0800

    fix merge syntax for webpack

--- webpack_conf/webpack.prod.js
+++ webpack_conf/webpack.prod.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const common = require('./webpack.common.js');
