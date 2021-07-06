ca2cee912822106467cbd94cc8a150751b93588e
michelle-mok <michellemokhuihui@gmail.com>
2021-07-06 15:06:11 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './styles.scss';
+import React from 'react';
+import { render } from 'react-dom';
+import data from './bugFile.json';
 
-console.log('hello');
+const jsxListItems = data.bugs.map((bug) => (
+  <li>
+    <p>
+      id:
+      {bug.id}
+    </p>
+    <p>
+      problem:
+      {bug.problem}
+    </p>
+    <p>
+      error text:
+      {bug.errorText}
+    </p>
+    <p>
+      created:
+      {bug.createdAt}
+    </p>
+    <p>
+      updated:
+      {bug.updatedAt}
+    </p>
+    <p>
+      user email:
+      {bug.User.email}
+    </p>
+    <p>
+      feature:
+      {bug.Feature.name}
+    </p>
+  </li>
+));
+
+const myEl = (
+  <ul>
+    {jsxListItems}
+  </ul>
+);
+
+const rootElement = document.createElement('div');
+
+document.body.appendChild(rootElement);
+
+render(myEl, rootElement);
--- webpack_conf/webpack.dev.js
+++ webpack_conf/webpack.dev.js
@@ -18,7 +18,7 @@ module.exports = merge(common, {
         use: {
           loader: require.resolve('babel-loader'),
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
