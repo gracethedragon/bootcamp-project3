f9000b4e06a7fb16672ca39e169ce8df92a329b0
Kai Yuan Neo <kai@rocketacademy.co>
2022-03-26 13:43:46 +0800

    fix bug in full example where Item model not initialised, hence not able to retrieve items

--- models/index.mjs
+++ models/index.mjs
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize';
 import url from 'url';
 import allConfig from '../config/config.js';
+import initItemModel from './item.mjs';
 
 const env = process.env.NODE_ENV || 'development';
 const config = allConfig[env];
@@ -27,6 +28,8 @@ if (env === 'production') {
 else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+db.Item = initItemModel(sequelize, Sequelize.DataTypes);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
